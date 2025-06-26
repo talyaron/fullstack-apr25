97e567ca4300653a8c893fc08144c155cec3bb78
Ayala Halevi <ayalahalevi2@gmail.com>
2025-06-26 20:01:26 +0300

    classpuzzle'

---
+++ 02-typescript/03-conditions-26-06-2025/classPuzzle/Ayala/index.ts
@@ -0,0 +1,57 @@
+let shani: number = 25;
+let udi: number = 78;
+let ori: number = 5;
+let rechel: number = 90;
+let noam: number = 45;
+
+let go: string = "you can go to the start you the queue";
+let wait: string = "you have to wait to your turn"
+
+if (shani >= 75) {
+    console.log("Shani" + go);
+
+}
+else {
+    console.log("Shani" + wait);
+
+}
+
+
+if (udi >= 75) {
+    console.log("Udi" + go);
+
+}
+else {
+    console.log("Udi" + wait);
+
+}
+
+
+if (ori >= 75) {
+    console.log("Ori" + go);
+
+}
+else {
+    console.log("Ori" + wait);
+
+}
+
+
+if (rechel >= 75) {
+    console.log("Rechel" + go);
+
+}
+else {
+    console.log("Rechel" + wait);
+
+}
+
+
+if (noam >= 75) {
+    console.log("Noam" + go);
+
+}
+else {
+    console.log("Noam" + wait);
+
+}
