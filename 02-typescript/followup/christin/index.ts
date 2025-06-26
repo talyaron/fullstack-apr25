663784fb2bce787fbe0013d9733cc51780e57b65
Christinjeries <christinjeries2@gmail.com>
2025-06-26 19:54:04 +0300

    class puzzle

---
+++ 02-typescript/followup/christin/index.ts
@@ -0,0 +1,11 @@
+let man = "Adam";
+let num: number = 42;
+let person = "I'm Christin";
+let age: number = 32;
+let per: number = 8;
+
+console.log(man);
+console.log(num);
+console.log(person);
+console.log(age);
+console.log(per);
