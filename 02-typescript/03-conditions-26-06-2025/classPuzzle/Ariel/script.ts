1d7dd44206a0b8c873e2385be532a412718bdaf4
Ariel71232 <ari71232@outlook.com>
2025-06-26 20:32:09 +0300

    CP finished

---
+++ 02-typescript/03-conditions-26-06-2025/classPuzzle/Ariel/script.ts
@@ -0,0 +1,18 @@
+let udiAge: number = 78;
+let shaniAge: number = 25;
+let oldAge = 75;
+
+if (udiAge >= oldAge) {
+  console.log(`you can move to the front of the queue!`);
+} else console.log(`you have to wait in line!`);
+
+if (shaniAge >= oldAge) {
+  console.log(`you can move to the front of the queue!`);
+} else console.log(`you have to wait in line!`);
+
+// let namePrompt = prompt(`Enter your name:`);
+// let agePrompt = Number(prompt(`Enter your age:`));
+
+// if (agePrompt >= oldAge) {
+//   console.log(`Hello! ${namePrompt}, you can move to the front of the queue!`);
+// } else console.log(`you have to wait in line!`);
