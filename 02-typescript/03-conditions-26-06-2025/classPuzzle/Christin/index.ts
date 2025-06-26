6cc3fb0445615701b718a7dcb14e082951508375
Christinjeries <christinjeries2@gmail.com>
2025-06-26 19:56:08 +0300

    class puzzle

--- 02-typescript/03-conditions-26-06-2025/classPuzzle/Christin/index.ts
+++ 02-typescript/03-conditions-26-06-2025/classPuzzle/Christin/index.ts
@@ -1,5 +1,7 @@
 let udi = 78;
 let shani = 25;
+let adam = 75;
+let shay = 85;
 
 if(udi > 75) { 
     console.log("go to the front of the line");
@@ -8,6 +10,17 @@ if(udi > 75) {
 }
 
 if(shani > 75) { 
+    console.log("go to the front of the line");
+} else {
+    console.log("stay in line");
+}
+
+if(adam > 75) { 
+    console.log("go to the front of the line");
+} else {
+    console.log("stay in line");
+}
+if(shay > 75) { 
     console.log("go to the front of the line");
 } else {
     console.log("stay in line");
