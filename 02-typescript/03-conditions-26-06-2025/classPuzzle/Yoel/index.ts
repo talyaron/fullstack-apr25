7f56a21b24cec9ceaeadb4f5ff315f336bfb948d
Yoel <rls.cimof@gmail.com>
2025-06-27 22:30:33 +0300

    work

---
+++ 02-typescript/03-conditions-26-06-2025/classPuzzle/Yoel/index.ts
@@ -0,0 +1,17 @@
+let hi : string = "Hello there!"
+let what: number= 42
+let Hello : string = "what's up"
+let old : string ="you can move to the front of the queue"
+let young : string = "you should stay at the back of the row like everybody else"
+
+const message: string = "Please type your age!";
+console.log(message);
+console.log(hi);
+
+let x : number = 74;
+if (x - 75 >= 0){
+    console.log(old)
+} else {
+    console.log(young);
+}
+
