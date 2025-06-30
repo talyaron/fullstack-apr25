96778bef1a86f4ae537c81a132dc97c9b9e74f22
sahar <saharshabat@gmail.com>
2025-06-30 19:54:39 +0300

    sahar

--- 02-typescript/05-functions-30-06-2025/classPuzzle/nativ&bar&sahar/index.ts
+++ 02-typescript/05-functions-30-06-2025/classPuzzle/nativ&bar&sahar/index.ts
@@ -1,19 +1,27 @@
 function isPrime(a: number) {
-  let i = 2;
   let prime = true;
   try {
-    if (a<2){
-      throw new Error("number can't be less than 2")
+    if (a % 1) {
+      throw new Error("not a int");
     }
-    else if (a % i === 0) {
-      prime = false; 
-      return "not prime" 
+    if (a < 2) {
+      throw new Error("number can't be less than 2");
     }
+    if (typeof a != "number") {
+      throw new Error("incorrect type");
+    }
+    
+    for (let i = 2; i < a; i++) {
+      if (a % i === 0) {
+        prime = false;
+        return prime;
+      }
+    }
+    if (prime) return true;
   } catch (e) {
     console.error("error", e);
   }
-  if(prime) return "is prime"
-
+  if (prime) return prime;
 }
 
-console.log("number:", isPrime(1))
+console.log("number:", isPrime("dgf"));
