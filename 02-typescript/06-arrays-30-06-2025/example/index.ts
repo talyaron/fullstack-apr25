d6582e9ae3644db2517b2cc7a021783d90e1fb5a
Tal <tal.yaron@gmail.com>
2025-06-30 21:19:20 +0300

    cont

--- 02-typescript/06-arrays-30-06-2025/example/index.ts
+++ 02-typescript/06-arrays-30-06-2025/example/index.ts
@@ -22,3 +22,11 @@ let myBooleans: boolean[] = [true, false, true];
 myNumbers.push(6); // add a number to the end of the array
 myStrings.push("new string"); // add a string to the end of the array
 myStrings.push("42"); // add another string to the end of the array
+
+//Methods on arrays
+console.log(myNumbers.length); // 6 - the length of the array
+console.log(myStrings.indexOf("hello")); // 0 - the index of the string "hello" in the array
+console.log(myNumbers.indexOf(4)); // 3 - the index of the number 4 in the array
+console.log(myStrings.includes("world")); // true - check if the string "world" is in the array
+console.log(myBooleans.includes(false)); // true - check if the boolean false is in
+console.log(myNumbers.slice(1, 3)); // [2, 3] - get a slice of the array from index 1 to index 3 (not inclusive)
