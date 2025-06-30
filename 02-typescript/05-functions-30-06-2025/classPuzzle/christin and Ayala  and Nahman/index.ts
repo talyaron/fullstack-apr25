418b3ba377ac4d46ec31f6145ed7f75335a463c4
Tal <tal.yaron@gmail.com>
2025-06-30 20:42:23 +0300

    arrays

--- 02-typescript/05-functions-30-06-2025/classPuzzle/christin and Ayala  and Nahman/index.ts
+++ 02-typescript/05-functions-30-06-2025/classPuzzle/christin and Ayala  and Nahman/index.ts
@@ -1,4 +1,4 @@
-function nPrime(a : number):boolean|undefined{
+function isPrime(a : number):boolean|undefined{
     try{
         if(a <= 1) throw new Error(a + " can't be a prime number, prime number has to be bigger than 1");
 
@@ -15,4 +15,8 @@ function nPrime(a : number):boolean|undefined{
     }
 }
 
-console.log("isPrime:", nPrime(7));
+console.log("isPrime:", isPrime(7));
+console.time("isPrime");
+console.log("isPrime (1971):", isPrime(197119711971));
+console.timeEnd("isPrime");
+console.log("isPrime (0):", isPrime(0));
