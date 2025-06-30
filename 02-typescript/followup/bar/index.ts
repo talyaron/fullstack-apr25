19a2a4d02f3b3413d34ba57f8516532711556626
bar <bari199900@gmail.com>
2025-06-30 18:36:44 +0300

    .
    .

---
+++ 02-typescript/followup/bar/index.ts
@@ -0,0 +1,35 @@
+//Level 1
+//Exrecise 1.1:
+let x = 12;
+console.log("12 % 3 =" + (x % 3));
+console.log("12 % 4 =" + (x % 4));
+console.log("12 % 5 =" + (x % 5));
+
+//Exrecise 1.2:
+let myAge: number = 26;
+let birthYear: number = 1999:
+let currentYear: number = myAge + birthYear;
+
+console.log("My age is: "+ myAge);
+console.log("My birth year is: "+ birthYear);
+console.log("The current year is: "+currentYear);
+
+//Exrecise 1.3:
+let a = 15;
+let b = 4;
+
+let sum = a + b;
+let difference = a - b;
+let product = a * b; 
+let quotient = a / b;
+let remainder = a % b;
+let power = a ** 2;
+
+console.log("Sum: " + sum);
+console.log("Difference: " + difference);
+console.log("Product: " + product);
+console.log("Quotient: "+ quotient);
+console.log("remainder: " + remainder);
+console.log("a to the power of 2: " + power);
+
+//Exrecise 1.4:
