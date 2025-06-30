47e8b456e5f1fb628523df29f7b24a664e81a325
bar <bari199900@gmail.com>
2025-06-30 18:36:44 +0300

    .
    .

---
+++ 02-typescript/05-functions-30-06-2025/followup/bar/index.ts
@@ -0,0 +1,19 @@
+function addNumbers(a: number, b: number): number {
+return a + b;
+}
+let result = addNumbers(5, 3);
+console.log("Result is:", result);
+
+function greet(name: string): string {
+ return "Hello, " + name + "!";   
+}
+
+let message = greet("Bar");
+console.log(message);
+
+function multiply(a: number, b: number): number {
+return a * b;    
+}
+
+let product = multiply (4, 6);
+console.log("The product is:", product);
