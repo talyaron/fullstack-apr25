90564f73fd88f15fecf9d5c94ee29b5197ccce6b
bar <bari199900@gmail.com>
2025-07-02 12:27:56 +0300

    keeping on with H.M

--- 02-typescript/followup/bar/index.ts
+++ 02-typescript/followup/bar/index.ts
@@ -33,3 +33,171 @@ console.log("remainder: " + remainder);
 console.log("a to the power of 2: " + power);
 
 //Exrecise 1.4:
+let num1 = 7;
+let num2 = 12;
+let num3 = 23;
+let num4 = 30;
+let num5 = 45;
+
+if (num1 % 2 ===0) {
+    console.log(num1 + "is even");
+} else {
+    console.log(num1 + "is odd");
+}
+
+//Level 2
+//Exrecise 2.1:
+for (let i = 1; i <= 20; i++) {
+    if (i % 3 === 0) { 
+        console.log("Fizz");
+    }else{ 
+        console.log(i);
+    }
+}
+
+//Exrecise 2.1 B:
+for (let i = 1; i <=50; i++) {
+    if (i % 7 === 1) {
+        console.log(i + "leaves a remainder of 1 when devided by 7");
+    }
+}
+
+//Exrecise 2.2:
+const test = 'some value';
+let test1 = 0;
+let test2 = -8;
+let test3 = 15;
+let test4 = 24;
+let test5 = -7;
+
+
+let test1 = 0;
+
+if (test1 > 0) {
+    console.log(test1 + "is positive");
+} else if (test1 < 0) {
+    console.log(test1 + "is negative");
+} else {
+    console.log(test1 + "is zero");
+}
+
+if (test1 !==0) {
+    if (test1 % 2===0) {
+        console.log(test1 + "is even");
+    } else {
+        console.log(test1 + "is odd");
+    }
+}
+
+if (test % 5=== 0) {
+    console.log(test1 + "is divisible by 5");
+}
+
+console.log("Square of" + test1 + "is:" + (test1 ** 2));
+console.log("Cube of" + test1 + "is:" + (test1 ** 3));
+
+
+let test2 = -8;
+
+if (test2 > -8) {
+    console.log(test2 + "is positive");
+} else if (test2 < -8) {
+    console.log(test2 + "is negative");
+} else {
+    console.log(test2 + "is zero");
+}
+
+if (test2 !==-8) {
+    if (test1 % 2===-8) {
+        console.log(test2 + "is even");
+    } else {
+        console.log(test2 + "is odd");
+    }
+}
+
+if (test2 % 5=== -8) {
+    console.log(test2 + "is divisible by 5");
+}
+
+console.log("Square of" + test2 + "is:" + (test2 ** 2));
+console.log("Cube of" + test2 + "is:" + (test2 ** 3));
+
+
+let test3 = 15;
+
+if (test3 > 15) {
+    console.log(test3 + "is positive");
+} else if (test3 < 0) {
+    console.log(test3 + "is negative");
+} else {
+    console.log(test3 + "is zero");
+}
+
+if (test3 !==15) {
+    if (test3 % 2===15) {
+        console.log(test1 + "is even");
+    } else {
+        console.log(test1 + "is odd");
+    }
+}
+
+if (test % 5=== 15) {
+    console.log(test3 + "is divisible by 5");
+}
+
+console.log("Square of" + test3 + "is:" + (test3 ** 2));
+console.log("Cube of" + test3 + "is:" + (test3 ** 3));
+
+
+let test4 = 24;
+
+if (test4 > 0) {
+    console.log(test4 + "is positive");
+} else if (test4 < 0) {
+    console.log(test4 + "is negative");
+} else {
+    console.log(test4 + "is zero");
+}
+
+if (test4 !==0) {
+    if (test4 % 2===0) {
+        console.log(test4 + "is even");
+    } else {
+        console.log(test4 + "is odd");
+    }
+}
+
+if (test % 5=== 24) {
+    console.log(test4 + "is divisible by 5");
+}
+
+console.log("Square of" + test4 + "is:" + (test4 ** 2));
+console.log("Cube of" + test4 + "is:" + (test4 ** 3));
+
+
+let test5 = -7;
+
+if (test5 > 0) {
+    console.log(test5 + "is positive");
+} else if (test5 < 0) {
+    console.log(test5 + "is negative");
+} else {
+    console.log(test5 + "is zero");
+}
+
+if (test5 !==0) {
+    if (test5 % 2===0) {
+        console.log(test5 + "is even");
+    } else {
+        console.log(test5 + "is odd");
+    }
+}
+
+if (test % 5=== 0) {
+    console.log(test5 + "is divisible by 5");
+}
+
+console.log("Square of" + test5 + "is:" + (test5 ** 2));
+console.log("Cube of" + test5 + "is:" + (test5 ** 3));
+
+//Exrecise 2.3:
