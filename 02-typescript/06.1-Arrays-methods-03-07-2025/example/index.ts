1af0b6de3bb595dec3a129eb6b9c27fc246d4c6e
Tal Yaron <tal.yaron@gmail.com>
2025-07-03 18:06:38 +0300

    followup - array -methods

---
+++ 02-typescript/06.1-Arrays-methods-03-07-2025/example/index.ts
@@ -0,0 +1,13 @@
+let names:Array<string> = ["Alice", "Bob", "Charlie"];
+
+let nameStrings:string = names.join(", "); // Join the array elements into a single string, separated by ", "
+console.log(nameStrings);
+
+let nameArray: Array<string> = nameStrings.split(", "); // Split the string back into an array using ", " as the separator
+console.log(nameArray);
+
+let name2: Array<string> = names.slice(0, 2);
+console.log(name2);
+console.log(names);
+
+//find another just to copy alice and bob.
