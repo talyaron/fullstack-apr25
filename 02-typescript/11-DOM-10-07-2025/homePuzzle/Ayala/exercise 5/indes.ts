490fcbd2d40bc447ae06eaf3fac75405144510a9
Ayala Halevi <ayalahalevi2@gmail.com>
2025-07-13 22:43:48 +0300

    home puzzle update

--- 02-typescript/11-DOM-10-07-2025/homePuzzle/Ayala/exercise 5/indes.ts
+++ 02-typescript/11-DOM-10-07-2025/homePuzzle/Ayala/exercise 5/indes.ts
@@ -12,7 +12,7 @@ function updateProductPrices(): void {
         const prices = document.querySelectorAll(".price")
         if (prices.length === 0) throw new Error("price element not found");
         prices.forEach(p => {
-            if (p instanceof HTMLElement && p) {
+            if (p instanceof HTMLElement) {
                 const current = p.textContent;
                 const sale = Number(current.replace(/\$/g, ''));
                 p.textContent = "Sale: $" + String(Math.floor(sale * 0.9))
@@ -22,26 +22,94 @@ function updateProductPrices(): void {
         }
         );
     } catch (error) {
-    console.error("Error update Product Prices: ",error);
-        }
+        console.error("Error update Product Prices: ", error);
+    }
 }
-updateProductPrices()
+
 
 function highlightProducts(): void {
     try {
         const productNmae = document.querySelectorAll(".product-name")
         if (productNmae.length === 0) throw new Error("product-name element not found");
         productNmae.forEach(p => {
-            if (p instanceof HTMLElement && p) {
-                p.textContent += "⭐";
-                p.style.background = getRandomColor() 
+            if (p instanceof HTMLElement) {
+                p.textContent = "⭐ "+p.textContent;
+                p.style.background = getRandomColor()
             }
         }
         );
     } catch (error) {
-    console.error("Error update Product Prices: ",error);
-        }
+        console.error("Errorhighlighting Products: ", error);
+    }
 }
+
+
+function generateSummary(): void {
+    try {
+        const products = document.querySelectorAll(".product-name")
+        if (products.length === 0) throw new Error("product-name element not found");
+        const prices = document.querySelectorAll(".price")
+        if (prices.length === 0) throw new Error("price element not found");
+        const summary = document.querySelector("#summary")
+        if (!summary) throw new Error("summary element not found");
+        let pricesSum = 0
+        prices.forEach(x => {
+            pricesSum += (Number(x.textContent?.replace(/\D/g, '')))
+        })
+        console.log(pricesSum);
+
+        if (summary instanceof HTMLElement) {
+            summary.textContent = (`There are ${products.length} product and the average price is: ${Math.floor(pricesSum / products.length)} `)
+        }
+
+
+    } catch (error) {
+        console.error("Error summary: ", error);
+    }
+}
+
+
+function applyRandomStyles(): void {
+    try {
+        const desc = document.querySelectorAll(".description")
+        if (desc.length === 0) throw new Error("description element not found");
+        desc.forEach(p => {
+            if (p instanceof HTMLElement) {
+
+                p.style.color = getRandomColor()
+            }
+        }
+        );
+
+        const productNmae = document.querySelectorAll(".product-name")
+        if (productNmae.length === 0) throw new Error("product-name element not found");
+        productNmae.forEach(p => {
+            if (p instanceof HTMLElement) {
+
+                p.style.padding = "0.5rem"
+                p.style.textAlign = "center"
+                p.style.width = "30%"
+            }
+        }
+        );
+    } catch (error) {
+        console.error("Error description color: ", error);
+    }
+}
+function setIntervalTitle() {
+    try {
+        const title = document.getElementById("page-title");
+        if (!title) throw new Error("Title element not found");
+        title.style.textAlign = "center"
+        setInterval(() => { title.style.background = getRandomColor() }
+            , 2000);
+    } catch (error) {
+        console.error("Error updating title:", error);
+    }
+}
+
+updateProductPrices()
 highlightProducts()
-
-
+generateSummary()
+applyRandomStyles()
+setIntervalTitle()
