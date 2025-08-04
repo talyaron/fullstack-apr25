46526cb24298c94076d8fd76e7278c63484fc302
amit reuveni <amitr123r@gmail.com>
2025-08-04 17:08:30 +0300

    add player and style

--- 02-typescript/16-classes-31-07-2025/homePuzzle/amit/index.ts
+++ 02-typescript/16-classes-31-07-2025/homePuzzle/amit/index.ts
@@ -1,3 +1,8 @@
+class Field {
+
+}
+
+
 class FootballPlayer {
     name: string;
     yearOfBirth: number;
@@ -7,13 +12,13 @@ class FootballPlayer {
     playerElement: HTMLElement;
     location: { x: number; y: number; };
 
-    constructor(name: string, year: number, position: string, team: string, imageUrl: string, domElement: HTMLElement, Location: { x: number, y: number}) {
+    constructor(name: string, year: number, position: string, team: string, imageUrl: string, domElement: HTMLElement, location: { x: number, y: number}) {
         this.name = name;
         this.yearOfBirth = year;
         this.position = position;
         this.team = team;
         this.imageUrl = imageUrl;
-        this.location = { x: 0, y: 0, };
+        this.location = location;
 
         if(domElement){
             this.render(domElement);
@@ -25,6 +30,9 @@ class FootballPlayer {
     render(container: HTMLElement) {
         this.playerElement = document.createElement("div");
         if(!this.playerElement) throw new Error("Can't find this player element");
+        this.playerElement.classList.add("player");
+
+        container.appendChild(this.playerElement);
     }
 
     move(x: number, y: number) {
