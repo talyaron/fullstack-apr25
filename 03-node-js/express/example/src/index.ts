801538ff455d610da9ebc553cbf1f798d87c278f
Tal Yaron <tal.yaron@gmail.com>
2025-08-18 23:01:52 +0300

    Initializes Express app with TypeScript
    Sets up a basic Express application using TypeScript, including configuration for nodemon, TypeScript compiler options, and initial route setup.
    
    This provides a foundation for building more complex applications using modern JavaScript features and type safety.

---
+++ 03-node-js/express/example/src/index.ts
@@ -0,0 +1,15 @@
+import express from 'express'
+const app = express()
+const port = 3000
+
+//api
+// /ariel == route
+app.get('/ariel', (_, res) => {
+
+  res.send('Hello World!')
+})
+
+app.listen(port, () => { //wait for api calls on port 3000
+  console.log(`Example app listening on port ${port}`)
+})
+
