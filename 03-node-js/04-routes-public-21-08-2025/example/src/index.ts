f14ecd39326d67434ebe6ce6b94099730f84cd55
Tal Yaron <tal.yaron@gmail.com>
2025-08-21 17:55:49 +0300

    fixed
    fixed

--- 03-node-js/04-routes-public-21-08-2025/example/src/index.ts
+++ 03-node-js/04-routes-public-21-08-2025/example/src/index.ts
@@ -3,7 +3,7 @@ import express from 'express';
 const app = express();
 const PORT = 3000;
 
-app.use(express.static('public'));
+app.use(express.static('./src/public'));
 
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
