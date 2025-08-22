7fdae5eabfed8ffce0ec51636f723524a5e70029
bar <bari199900@gmail.com>
2025-08-22 13:41:06 +0300

    updating

---
+++ 02-typescript/12-Model-view-14-7-2025/homePuzzle-Project/bar/level-3/index.ts
@@ -0,0 +1,6 @@
+const canvas = document.getElementById('gameCanvas');
+const ctx = canvas.getContext('2d');
+const scoreElement = document.getElementById('score');
+const highScoreElement = document.getElementById('highScore');
+ const gameOverElement = document.getElementById('gameOver');
+ const finalScoreElement = document.getElementById('finalScore');
