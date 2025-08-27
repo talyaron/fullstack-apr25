8e83fe5bd7285e1419f3a0217f7d1afeaa19f221
Ayala Halevi <ayalahalevi2@gmail.com>
2025-08-27 21:32:41 +0300

    homepuzzle

--- 03-node-js/04-routes-public-21-08-2025/homePuzzle/Ayala/src/index.ts
+++ 03-node-js/04-routes-public-21-08-2025/homePuzzle/Ayala/src/index.ts
@@ -3,7 +3,7 @@ import { products } from "./model/productsData";
 
 const app = express();
 const PORT = 5000;
-
+app.use(express.json());
 app.use(express.static("./src/public"));
 
 //API
@@ -20,7 +20,19 @@ app.get(`/product/get-amount`, (_, res) => {
     res.status(500).send({ error: `Internal Server Error` });
   }
 });
-
+app.get(`/product/get-average-price`, (_, res) => {
+  try {
+    if (!products || products.length === 0) {
+      res.status(404).send({ error: "no products found" });
+      throw new Error("no products found");
+    }
+    const averegePrice = (products.reduce((a: number, b) => a + b.price, 0) / products.length).toFixed(2);
+    res.status(200).send({ averegePrice });
+  } catch (error) {
+    console.error("Error occurred while fetching products count:", error);
+    res.status(500).send({ error: `Internal Server Error` });
+  }
+});
 app.get(`/product/get-products`, (_, res) => {
   try {
     if (!products || products.length === 0) {
@@ -34,7 +46,20 @@ app.get(`/product/get-products`, (_, res) => {
     res.status(500).send({ error: `Internal Server Error` });
   }
 });
+app.post(`/product/add-product`, (req, res) => {
+  try {
+    const {body} = req;
+    const {name, price, category, image, stock} = body;
+    if(!name||!price||!category||!image||!stock){
+        res.status(400).send({error:"missing product information"});
+        throw new Error("missing product information");
+    }
 
+
+  } catch (error) {
+    console.error("Error occurred while fetching from server add-product: ", error);
+  }
+});
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
