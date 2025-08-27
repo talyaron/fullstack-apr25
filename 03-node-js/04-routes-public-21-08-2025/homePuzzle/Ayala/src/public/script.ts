7622abaff4f3c06117ef51baf30ed6995d2bf352
Ayala Halevi <ayalahalevi2@gmail.com>
2025-08-27 21:32:41 +0300

    homepuzzle

--- 03-node-js/04-routes-public-21-08-2025/homePuzzle/Ayala/src/public/script.ts
+++ 03-node-js/04-routes-public-21-08-2025/homePuzzle/Ayala/src/public/script.ts
@@ -1,26 +1,73 @@
 // import { Product } from "../model/productModel";
-
+//main controller
 async function main() {
   try {
+    const form = document.getElementById("add-product");
+    if (!form) throw new Error("add-product form element not found");
+    form.addEventListener("submit", handleSubmit);
+
     const productsAmount = await showAmount();
-    console.log(productsAmount);
-    const elementInHTML = document.querySelector("#products-amount");
-    if (!elementInHTML) throw new Error("products-amount elemet not found");
-    elementInHTML.innerHTML = productsAmount.toString();
+
+    const amountElement = document.querySelector("#products-amount");
+    if (!amountElement) throw new Error("products-amount elemet not found");
+    amountElement.innerHTML = productsAmount.toString();
+
     const products = await getProducts();
     if (!products) throw new Error("Error geting products list in main function");
     renderProducts(products);
+
+    const avragePriceElement = document.querySelector("#average-price");
+    if (!avragePriceElement) throw new Error("average-price elemet not found");
+    const averageResponse = await getAvaragePrice();
+    avragePriceElement.innerHTML = averageResponse.toString();
   } catch (error) {
     console.error("error: ", error);
   }
 }
 main();
 
+//control
+async function handleSubmit(event: Event) {
+  try {
+    event.preventDefault(); //*
+    console.log("Form submitted");
+    if (!(event.target instanceof HTMLFormElement)) throw new Error("Event target is not a form");
+    const formData = new FormData(event.target); //*
+    // const data = Object.fromEntries(formData.entries()); //*
+const name = formData.get("name")
+const stock = formData.get("stock")
+const price = formData.get("price")
+const category = formData.get("category")
+const image = formData.get("image")
+
+// const response = await fetch("")
+
+    // const dataname = String(formData.get("name") ?? "");
+    // const datastock = Number(formData.get("stock"));
+    // const dataprice = Number(formData.get("price"));
+    // const datacategory = String(formData.get("category") ?? "");
+    // const dataimg = String(formData.get("img") ?? "");
+    // if (!dataimg || !dataname || !datacategory || !datastock || isNaN(datastock) || !dataprice || isNaN(dataprice))
+    //   throw new Error("Invalid form data");
+
+    // const newProduct: Product = {
+    //   name: dataname,
+    //   stock: datastock,
+    //   price: dataprice,
+    //   category: datacategory,
+    //   img: dataimg,
+    // };
+
+  } catch (error) {
+    console.error("Error handeling submit form add-produts: ", error);
+  }
+}
+
 interface AmountResponse {
   productAmount: number;
   error?: string;
 }
-
+//service
 async function showAmount(): Promise<number> {
   try {
     const response = await fetch("http://localhost:5000/product/get-amount");
@@ -63,8 +110,28 @@ async function getProducts(): Promise<Product[]> {
     return [];
   }
 }
+interface AverageResponse {
+  averegePrice: number;
+  error?: string;
+}
+
+async function getAvaragePrice(): Promise<number> {
+  try {
+    const response = await fetch("http://localhost:5000/product/get-average-price");
+
+    const data: AverageResponse = (await response.json()) as AverageResponse;
+    console.log(data);
+
+    if (!response.ok || data.error) throw new Error(data.error || "Error");
+
+    return data.averegePrice;
+  } catch (error) {
+    console.error("Error occurred while fetching student count:", error);
+    return 0; // Return 0 or handle the error as needed
+  }
+}
 //view
-async function renderProducts(products: Product[]) {
+function renderProducts(products: Product[]) {
   try {
     const productsContainer = document.getElementById("productsContainer");
     if (!productsContainer) throw new Error("productsContainer element not found.");
@@ -75,7 +142,7 @@ async function renderProducts(products: Product[]) {
       const stockClass = p.stock > 10 ? "in-stock" : p.stock === 0 ? "out-of-stock" : "low-stock";
       const stock = p.stock > 10 ? "In Stock" : p.stock === 0 ? "Out of Stock" : "Low Stock";
       productelement.innerHTML = `
-        <div class="product_image"  ></div>
+        <div class="product_image" style="background-image: url(${p.img})"></div>
         <div class="product_name">${p.name}</div>
         <div class="product_price">${p.price} $</div>
         <div class="product_category">${p.category}</div>
@@ -91,14 +158,24 @@ async function renderProducts(products: Product[]) {
 }
 
 //model- view
-function productsHtml(product: Product): string {
-  return `
- <div class="product">
-        <div class="product_image" style="background-image: url(${product.img})"></div>
-        <div class="product_name">${product.name}</div>
-        <div class="product_price">${product.price} $</div>
-        <div class="product_category">${product.category}</div>
-        <div class="product_stock">${product.stock} in stock</div>
-      </div>
-`;
-}
+// function productsHtml(product: Product): string {
+//   return `
+//  <div class="product">
+//         <div class="product_image" style="background-image: url(${product.img})"></div>
+//         <div class="product_name">${product.name}</div>
+//         <div class="product_price">${product.price} $</div>
+//         <div class="product_category">${product.category}</div>
+//         <div class="product_stock">${product.stock} in stock</div>
+//       </div>
+// `;
+// }
+
+// async function getAvaragePrice(products: Product[]): Promise<number> {
+//   try {
+//     if (!products || products.length === 0) throw new Error("no products found");
+//     return parseFloat((products.reduce((a: number, b) => a + b.price, 0) / products.length).toFixed(2));
+//   } catch (error) {
+//     console.error("Error clac arevage price: ", error);
+//     return 0;
+//   }
+// }
