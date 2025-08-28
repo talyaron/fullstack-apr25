e9f2bffcc35683dca6b3d0ebaaed534f24e89166
amit reuveni <amitr123r@gmail.com>
2025-08-28 18:12:12 +0300

    change the name of the folder

--- 03-node-js/06-get-post-class-project-28-08-2025/amit-valeriya/src/index.ts
+++ 03-node-js/06-get-post-class-project-28-08-2025/amit-valeriya/src/index.ts
@@ -1,5 +1,4 @@
 import express from 'express';
-import { students } from './model/studentsData';
 
 const app = express();
 const PORT = 3000;
@@ -9,39 +8,6 @@ const PORT = 3000;
 app.use(express.static('./src/public'));
 
 
-//API route (for data)
-app.get('/students/number-of-students', (_, res) => {
-    try {
-
-        if (!students || students.length === 0) {
-            res.status(404).send({ error: 'No students found' });
-
-            return;
-        }
-        // Simulating some processing
-        const numberOfStudents = students.length;
-
-        res.status(200).send({ numberOfStudents });
-
-    } catch (error: any) {
-        console.error('Error occurred while fetching student count:', error);
-        res.status(500).send({ error: `Internal Server Error: ${error.message}` });
-    }
-});
-
-app.get("/students/get-all-students", (_, res) => {
-    try {
-        if (!students || students.length === 0) {
-            res.status(404).send({ error: 'No students found' });
-            return;
-        }
-
-        res.status(200).send({ students });
-    } catch (error: any) {
-        console.error('Error occurred while fetching all students:', error);
-        res.status(500).send({ error: `Internal Server Error: ${error.message}` });
-    }
-});
 
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
