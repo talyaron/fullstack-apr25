e2067144a83194a229f8388d3ef22d560ecf61df
amit reuveni <amitr123r@gmail.com>
2025-08-28 19:20:27 +0300

    add html and style.scss

--- 03-node-js/06-get-post-class-project-28-08-2025/amit-valeriya/src/model/moviesData.ts
+++ 03-node-js/06-get-post-class-project-28-08-2025/amit-valeriya/src/model/moviesData.ts
@@ -3,5 +3,6 @@ import { Movie } from "./moviesModel";
 export const movies: Movie[] = [
     { id: 1, title: "Inception", director: "Christopher Nolan", year: 2010 },
     { id: 2, title: "The Matrix", director: "Lana Wachowski, Lilly Wachowski", year: 1999 },
-    { id: 3, title: "Interstellar", director: "Christopher Nolan", year: 2014,}
+    { id: 3, title: "Interstellar", director: "Christopher Nolan", year: 2014 },
+    { id: 4, title: "The Dark Knight", director: "Christopher Nolan", year: 2008 }
 ];
