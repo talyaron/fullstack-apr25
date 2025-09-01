f834f87b7ded9eb15b6d3bac50b7ef5fb72f913d
Razanmani <razan.mani97@gmail.com>
2025-09-01 21:03:08 +0300

    finished classPuzzle

---
+++ 03-node-js/07-Update-delete-1-9-2025/classPuzzle/Razan/src/model/MoviesModle.ts
@@ -0,0 +1,10 @@
+export interface Movie {
+    id: string ;
+    backgroundColor: string;
+    title: string;
+    year: number;
+    genre: string;
+    rating: number;
+    poster: string;
+    description: string;
+}
