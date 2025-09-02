a71d6e4a2c14354113e99dd96e33d20db814e05b
Ariel71232 <ari71232@outlook.com>
2025-09-02 22:26:52 +0300

    Halfway done!
    created endpoints for get all tasks, get id itself, create a new task by client!

--- 03-node-js/07-Update-delete-1-9-2025/classPuzzle/Ariel/src/model/MoviesModle.ts
+++ 03-node-js/07-Update-delete-1-9-2025/classPuzzle/Ariel/src/model/MoviesModle.ts
@@ -6,10 +6,5 @@ export interface Movie {
     rating: number;
     poster: string;
     description: string;
-<<<<<<<< HEAD:03-node-js/07-Update-delete-1-9-2025/classPuzzle/Ariel/src/model/MoviesModle.ts
     color?: string;
 } 
-========
-    customColor?: string;
-}
->>>>>>>> origin/main:03-node-js/07-Update-delete-1-9-2025/classPuzzle/val/src/model/MoviesModle.ts
