f0f2212ed2c0f08951504500f3fed35c90abedce
Ayala Halevi <ayalahalevi2@gmail.com>
2025-09-05 17:13:04 +0300

    starting home puzzle

---
+++ 03-node-js/07-Update-delete-1-9-2025/Home-Puzzle/Ayala/src/public/script.ts
@@ -0,0 +1,17 @@
+
+//main controller
+async function main() {
+try {
+
+} catch (error) {
+    console.error("error in main controller: ", error);
+
+}
+}
+main()
+//control functions
+
+
+//view functions
+
+//servers
