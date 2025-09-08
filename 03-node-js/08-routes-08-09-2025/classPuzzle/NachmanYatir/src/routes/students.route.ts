97fed7cb9a59719d9d61e502590039bdf08d990b
matania1231 <mataniyauzan123@gmail.com>
2025-09-08 20:59:39 +0300

    finished classpuzzle #2
    please fix the remaining problems

--- 03-node-js/08-routes-08-09-2025/classPuzzle/NachmanYatir/src/routes/students.route.ts
+++ 03-node-js/08-routes-08-09-2025/classPuzzle/NachmanYatir/src/routes/students.route.ts
@@ -1,44 +1,11 @@
 import express from 'express'
 import { Student, students } from '../model/student.model';
+import { addStudent, getAllStudents, updateStudentById } from '../controllers/students.controllers';
 const router = express.Router();
 
-router.get("/all-students", (_req, res) => {
-    res.status(200).send({ students });
-});
-
-router.post("/add-student", (req, res) => {
-    console.log("Request Body:", req.body);
-    const newStudent = req.body as Student;
-    newStudent.id = students.length + 1; 
-
-    students.push(newStudent);
-    res.status(201).send(`Student ${newStudent.name} added successfully`);
-});
-
-router.patch("/update-student/:id", (req, res) => {
-    try {
-        const numericId = Number(req.params.id);
-        const student = students.find(student => student.id === numericId);
-        
-        if (!student) {
-            res.status(404).send({ error: "student not found" });
-            return;
-        }
-
-
-        if (req.body.age !== undefined) {
-            student.age = Number(req.body.age);
-        }
-        if (req.body.name !== undefined) {
-            student.name = req.body.name;
-        }
-
-        
-        res.status(200).send({ ok: true, student: student });
-        
-    } catch (error) {
-        console.error(error, "initial server error");
-        res.status(500).send({ error: true });
-    }
-});
-export default router;  
+router
+    .get("/all-students", getAllStudents)
+    .post("/add-student", addStudent)
+    .patch("/update-student/:id", updateStudentById);
+    
+    export default router;
