af1842e161aad9709b66f533480c6e74717d77d4
Ariel71232 <ari71232@outlook.com>
2025-09-08 21:13:53 +0300

    Bring back the example files!

---
+++ 03-node-js/07-Update-delete-1-9-2025/example/src/model/MoviesData.ts
@@ -0,0 +1,58 @@
+// Here, we create a simple in-memory data store for movies
+// In a real-world application, this would be replaced with a database
+
+import { Movie } from "./MoviesModle";
+
+export const movies: Movie[] = 
+[
+  { 
+    id: "1",
+    title: "Inception",
+    year: 2010,
+    genre: "Action",
+    rating: 4,
+    poster: "https://encrypted-tbn3.gstatic.com/images?q=tbn:ANd9GcRRyuWmayVBvqjd1MxTKpRgauq2cCtUzb7Q9QvaFTkAuxAU_EYMoCE3wBuJeftxIzf0grreIw",
+    description: "A thief who steals corporate secrets through dream-sharing technology is tasked with planting an idea into the mind of a CEO.",
+    color: "white"
+  },
+  {
+    id: "2",
+    title: "The Dark Knight",
+    year: 2008,
+    genre: "Action",
+    rating: 4,
+    poster: "https://encrypted-tbn3.gstatic.com/images?q=tbn:ANd9GcTfE_qrYMBZ_JB8om-34WGaZARhpX26yWRttqIDvn4_7l--UzX8mxKcPrc59IcvTpEA_G8gPA",
+    description: "Batman faces the Joker, a criminal mastermind who plunges Gotham into anarchy and forces the hero closer to crossing the fine line between hero and vigilante.",
+    color: "white"
+  },
+  {
+    id: "3",
+    title: "Interstellar",
+    year: 2014,
+    genre: "Adventure",
+    rating: 4,
+    poster: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSngBJ0B7UDrLUkDlp6DCQLsEYuWR-DiHwbnxFFCniB3HiP3f3NZmR1-lKSC34ge6YXu4LX",
+    description: "A team of explorers travel through a wormhole in space in an attempt to ensure humanity's survival.",
+    color: "white"
+  },
+  {
+    id: "4",
+    title: "Parasite",
+    year: 2019,
+    genre: "Comedy",
+    rating: 4,
+    poster: "https://encrypted-tbn1.gstatic.com/images?q=tbn:ANd9GcTTRGm5Vxt-AKoe72ASaC0F1w58TkuIQTuYrjrzhHkAcZYXXUS9WQdAuaikkuRMX50MWN01l",
+    description: "Greed and class discrimination threaten the newly formed symbiotic relationship between the wealthy Park family and the destitute Kim clan.",
+    color: "white"
+  },
+  {
+    id: "5",
+    title: "The Shawshank Redemption",
+    year: 1994,
+    genre: "Drama",
+    rating: 4,
+    poster: "https://encrypted-tbn3.gstatic.com/images?q=tbn:ANd9GcSf1DK32xKMQzqSl8wnY1BLVu_gdwsRYzVSNM6A03r6c-fEwrif8raKzkFRuerw1KHdDICvOw",
+    description: "Two imprisoned men bond over years, finding solace and eventual redemption through acts of common decency.",
+    color: "white"
+  }
+];
