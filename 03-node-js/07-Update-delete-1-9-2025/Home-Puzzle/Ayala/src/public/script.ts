2731c65844d56f36c045dca53e5b4dbc8f8caffc
Ayala Halevi <ayalahalevi2@gmail.com>
2025-09-08 14:54:21 +0300

    level 1 fixing bug of delete and complete in added taskes

--- 03-node-js/07-Update-delete-1-9-2025/Home-Puzzle/Ayala/src/public/script.ts
+++ 03-node-js/07-Update-delete-1-9-2025/Home-Puzzle/Ayala/src/public/script.ts
@@ -12,8 +12,8 @@ interface Task {
 async function main() {
     try {
         await renderTasks(await getAllTasks())
-        const form = document.getElementById("addTaskForm");
-        if (!form) throw new Error("addTaskForm form element not found.");
+        const form = document.getElementById("taskForm");
+        if (!form) throw new Error("taskForm form element not found.");
         form.addEventListener("submit", await handleSubmit);
     } catch (error) {
         console.error("error in main controller: ", error);
@@ -33,14 +33,30 @@ async function handleSubmit(event: SubmitEvent) {
         const createdAt = new Date();
         const completed = false
         console.log(title, description, createdAt, completed);
+
+        const response = await fetch("http://localhost:2500/tasks/add-task", {
+            method: "POST",
+            headers: { "Content-Type": "application/json" },
+            body: JSON.stringify({ title, description, completed, createdAt }),
+        });
+        const data = await response.json();
+        if (!response.ok) throw new Error("error fetching add-task", (data.error));
+
+        event.target.reset();
+        main()
+        handleHideForm()
     } catch (error) {
         console.error('error in sbmiting new task: ', error);
 
     }
 }
 
+//* not working for new tasks
 async function handleComplete(id: string, event: Event) {
     try {
+        console.log(id);
+
+
         if (!(event.target instanceof HTMLInputElement)) throw new Error("error in the checkbox input");
         const isComplete: boolean = event.target.checked
         const res = await fetch('http://localhost:2500/tasks/mark-complete', {
@@ -58,16 +74,36 @@ async function handleComplete(id: string, event: Event) {
     } catch (error) {
         console.error('error in marking complete check box');
     }
-
 }
+function handleShowForm() {
+    try {
+        const form = document.getElementById("addTaskForm");
+        if (!form) throw new Error("addTaskForm form element not found.");
+        form.style.display = "flex"
+    } catch (error) {
+        console.error('error in showing add task form');
+
+    }
+}
+function handleHideForm() {
+    try {
+        const form = document.getElementById("addTaskForm");
+        if (!form) throw new Error("addTaskForm form element not found.");
+        form.style.display = "none"
+    } catch (error) {
+        console.error('error in showing add task form');
+
+    }
+}
+//error fetching the api: tasks/delete-task
 async function handleDelete(id: string) {
     try {
-
         const res = await fetch('http://localhost:2500/tasks/delete-task', {
             method: 'DELETE',
             headers: { 'Content-Type': 'application/json' },
             body: JSON.stringify({ id }) //data
         });
+
         const data = await res.json();
         if (!res.ok) {
             throw new Error('Failed to delete task:', data.error);
@@ -81,6 +117,11 @@ async function handleDelete(id: string) {
 interface TasksResponce {
     tasks: Task[];
     error?: string;
+
+}
+interface TaskResponce {
+    task: Task;
+    error?: string;
 }
 //servers
 async function getAllTasks(): Promise<Task[]> {
@@ -90,12 +131,22 @@ async function getAllTasks(): Promise<Task[]> {
         if (!response.ok || data.error) throw new Error(data.error || "unknown error");
         return data.tasks;
     } catch (error) {
-        console.error("error while fetching tasks from server");
+        console.error("error while fetching tasks from server: ", error);
         return [];
     }
 }
 
-
+async function getTask(id: string): Promise<Task> {
+    try {
+        const response = await fetch(`http://localhost:2500/tasks/get-task/:${id}`);
+        const data: TaskResponce = (await response.json()) as TaskResponce;
+        if (!response.ok || data.error) throw new Error(data.error || "unknown error");
+        return data.task;
+    } catch (error) {
+        console.error("error while fetching tasks from server: ", error);
+        return { id: "1", title: "Buy groceries", completed: false, createdAt: new Date(), description: "Milk, eggs, bread" };
+    }
+}
 //view functions
 
 async function renderTasks(tasks: Task[]) {
@@ -116,22 +167,22 @@ function taskHtml(task: Task): string {
       <div class="task-card__actions">
         <label class="task-card__complete">
           ${task.completed ? "Done</i>" : "Mark if done"}
-          <input type="checkbox" name="complete"  ${task.completed ? "checked" : ""}  onchange="handleComplete(${task.id}, event)">
+          <input type="checkbox" name="complete"  ${task.completed ? "checked" : ""}  onchange="handleComplete('${task.id}', event)">
         </label>
-        <button class="task-card__delete" onclick = "handleDelete(${task.id})">Delete</button>
+        <button class="task-card__delete" onclick = "handleDelete('${task.id}')">Delete</button>
       </div>
 
       <div class="task-card__info">
         <div class="task-card__date"> started at: ${task.createdAt}</div>
         <h2 class="task-card__title">${task.title}</h2>
-        <p class="task-card__description">${task.description}</p>
+        <p class="task-card__description">${task.description ? task.description : ""}</p>
       </div>
     </div>
     `
 }
-try {
+// try {
 
-} catch (error) {
-    console.error();
+// } catch (error) {
+//     console.error();
 
-}
+// }
