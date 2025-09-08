7835ac2dd64123b300ad0ed227221683897b354a
amit reuveni <amitr123r@gmail.com>
2025-09-08 18:59:28 +0300

    .

--- 03-node-js/08-routes-08-09-2025/classPuzzle/AyalaandAmitandChristin/src/routes/students.route.ts
+++ 03-node-js/08-routes-08-09-2025/classPuzzle/AyalaandAmitandChristin/src/routes/students.route.ts
@@ -3,6 +3,7 @@ import { Student, students } from '../model/student.model';
 const router = express.Router();
 
 router.get("/all-students", (_req, res) => {
+    console.log("GET /all-students called");
     res.status(200).send({ students });
 });
 
@@ -15,15 +16,26 @@ router.post("/add-student", (req, res) => {
     res.status(201).send(`Student ${newStudent.name} added successfully`);
 });
 
-router.put("/update-age/:id", (req, res)=>{
-    const {id} = req.params;
-    const age = req.body;
-    if(!id) return
-    const numericId = Number(id);
-    const student = students.find(student => student.id === numericId)
-    if(!student) return
-    student.age = Number(age);
-    res.status(200).send({ok:true})
+router.patch("/update-age/:id", (req, res) => {
+    try {
+        const { id } = req.params;
+        const age = req.body;
+        if (!id) res.status(404).send({ error: "id not found" });
+
+        const numericId = Number(id);
+        const student = students.find(student => student.id === numericId)
+        if (!student) {
+            res.status(404).send({ error: "student not found" })
+            return;
+        };
+
+        student.age = Number(age);
+        res.status(200).send({ ok: true })
+
+    } catch (error) {
+        console.error(error, "initial server error")
+        res.status(500).send({ error });
+    }
 })
 
 export default router;
