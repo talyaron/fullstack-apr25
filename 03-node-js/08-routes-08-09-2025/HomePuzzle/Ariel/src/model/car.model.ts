3c18378e5aa80f114715dcfa4317d9d295a54231
Ariel71232 <ari71232@outlook.com>
2025-09-10 21:56:09 +0300

    Level 2 started, not done yet 🏃‍➡️
    Controller's created, validation is remaining (& business logical).

--- 03-node-js/08-routes-08-09-2025/HomePuzzle/Ariel/src/model/car.model.ts
+++ 03-node-js/08-routes-08-09-2025/HomePuzzle/Ariel/src/model/car.model.ts
@@ -1,13 +1,54 @@
+export enum CarStatus {
+  AVAILABLE = "AVAILABLE",
+  SOLD = "SOLD",
+  RESERVED = "RESERVED",
+}
+
 export interface Car {
   id?: number;
   brand: string;
   model: string;
   year: number;
   price: number;
+  color: string;
+  mileage: number;
+  isAvailable: boolean;
+  status: CarStatus;
 }
 
 export const Cars: Car[] = [
-  { id: 1, brand: "Toyota", model: "Corolla", year: 2020, price: 20000 },
-  { id: 2, brand: "Honda", model: "Civic", year: 2019, price: 22000 },
-  { id: 3, brand: "Ford", model: "Mustang", year: 2021, price: 30000 },
+  {
+    id: 1,
+    brand: "Toyota",
+    model: "Corolla",
+    year: 2020,
+    price: 20000,
+    color: "Red",
+    mileage: 15000,
+    isAvailable: true,
+    status: CarStatus.AVAILABLE,
+  },
+
+  {
+    id: 2,
+    brand: "Honda",
+    model: "Civic",
+    year: 2019,
+    price: 22000,
+    color: "Blue",
+    mileage: 12000,
+    isAvailable: true,
+    status: CarStatus.AVAILABLE,
+  },
+  {
+    id: 3,
+    brand: "Ford",
+    model: "Mustang",
+    year: 2021,
+    price: 30000,
+    color: "Black",
+    mileage: 8000,
+    isAvailable: false,
+    status: CarStatus.SOLD,
+  },
 ];
