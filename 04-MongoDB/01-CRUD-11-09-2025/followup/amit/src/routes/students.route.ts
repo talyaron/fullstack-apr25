49c017333157c6990d21eef61ee58cc6f37f7b0b
amit reuveni <amitr123r@gmail.com>
2025-09-11 20:58:39 +0300

    .

--- 04-MongoDB/01-CRUD-11-09-2025/followup/amit/src/routes/students.route.ts
+++ 04-MongoDB/01-CRUD-11-09-2025/followup/amit/src/routes/students.route.ts
@@ -5,7 +5,7 @@ const router = express.Router();
 
 router.get("/all-students", studentController.getAllStudents);
 router.post("/add-student", studentController.addStudent);
-router.put("/update-student", studentController.updateStudent);
+router.patch("/update-student", studentController.updateStudent);
 router.delete("/delete-student", studentController.deleteStudent);
 
 export default router;
