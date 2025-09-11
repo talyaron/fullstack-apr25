dc7ee06e9e208c430ac85a815d38446d2d01e435
Ayala Halevi <ayalahalevi2@gmail.com>
2025-09-11 16:32:38 +0300

    level 2

--- 03-node-js/08-routes-08-09-2025/HomePuzzle/Ayala/src/controllers/cars.controllers.ts
+++ 03-node-js/08-routes-08-09-2025/HomePuzzle/Ayala/src/controllers/cars.controllers.ts
@@ -1,6 +1,8 @@
-import { Car, cars } from "../model/car.model.js";
+import { Car, cars } from "../model/car.model";
 import { Request, Response } from "express";
 
+const currentYear = new Date().getFullYear();
+//get
 export function getAllCars(_req: Request, res: Response) {
     try {
         if (!cars) return res.status(404).send({ error: "no cars found in data" })
@@ -10,26 +12,67 @@ export function getAllCars(_req: Request, res: Response) {
     }
 }
 
+export function getCarById(req: Request, res: Response) {
+    try {
+        const carId = parseInt(req.params.id, 10);
+        if (isNaN(carId)) return res.status(400).send({ error: "invalid car id" })
+        const car = cars.find(c => c.id === carId)
+        if (!car) return res.status(404).send({ error: "could not find car" })
+        res.status(200).send({ car })
+    } catch (error) {
+        res.status(500).send({ error: "unknown error in getCarById" });
+    }
+}
+export function getAvailableCars(_req: Request, res: Response) {
+    try {
+        if (!cars) return res.status(404).send({ error: "no cars found in data" })
+        const availableCars = cars.filter(car => car.isAvailable)
+        res.status(200).send(availableCars ? { availableCars } : { noCars: "no availble cars" });
+    } catch (error) {
+        res.status(500).send({ error: "unknown error in getAllCars" });
+    }
+}
+export function searchCars(req: Request, res: Response) {
+    try {
+        const carBrand = req.query.brand;
+        if (!carBrand) return res.status(400).send({ error: "invalid car brand" });
+        const brandedCars = cars.filter(car => car.brand === carBrand);
+        res.status(200).send(brandedCars ? { brandedCars } : { noCars: `no ${carBrand} cars` });
+    } catch (error) {
+        res.status(500).send({ error: "unknown error in searchCars" });
+
+    }
+}
+//post
 export function addCar(req: Request, res: Response) {
     try {
         console.log("Request Body:", req.body);
-        const newCar = req.body as Car;
-        newCar.id = cars.length + 1;
-        if (!newCar.brand || typeof newCar.brand !== "string"
-            || !newCar.model || typeof newCar.model !== "string"
-            || !newCar.year || typeof newCar.year !== "number"
-            || !newCar.price || typeof newCar.price !== "number") {
-            return res.status(400).send({ error: "Invalid car data" });
-        }
+        const { brand, model, year, price, color, mileage, isAvailable } = req.body;
+        // const newCar = req.body as Car;
+        const carid = cars.length + 1;
+        if (!brand || typeof brand !== "string") return res.status(400).send({ error: "Invalid car brand" });
+        if (!model || typeof model !== "string") return res.status(400).send({ error: "Invalid car model" });
+        if (year > currentYear || year < 1900 || typeof year !== "number") return res.status(400).send({ error: "Invalid car year" });
+        if (price < 0 || typeof price !== "number") return res.status(400).send({ error: "Invalid car price" });
+        if (mileage < 0) return res.status(400).send({ error: "Invalid car mileage" });
+        // {
+        //     return res.status(400).send({ error: "Invalid car data" });
+        // }
+        const newCar: Car = { id: carid, brand, model, year, price, color, mileage, isAvailable };
         cars.push(newCar);
-        res.status(201).send(`Student ${newCar} added successfully`);
+        res.status(201).send(`car ${newCar.id} added successfully`);
+        console.log(newCar);
 
     } catch (error) {
         res.status(500).send({ error: "unknown error in addCar" });
     }
 }
+
+//patch
 export function updateCarPrice(req: Request, res: Response) {
     try {
+        if (!cars) return res.status(404).send({ error: "no cars found in data" })
+
         const { id, price } = req.body;
 
         if (typeof id !== 'number' || typeof price !== 'number') {
@@ -46,5 +89,39 @@ export function updateCarPrice(req: Request, res: Response) {
         res.status(200).send(`car ${id} updated successfully`);
     } catch (error) {
         res.status(500).send({ error: `unknown error in update price` });
+    }
+}
+
+//put
+export function updateValue(_req: Request, res: Response) {
+    try {
+        if (!cars) return res.status(404).send({ error: "no cars found in data" })
+
+        cars.forEach(car => {
+            const age = currentYear - car.year;
+            let depreciatedPrice = car.price * (1 - 0.05 * age);
+            const mileageFactor = Math.floor(car.mileage / 10000) * 0.02;
+            depreciatedPrice *= (1 - mileageFactor);
+            if (age > 5) depreciatedPrice *= 0.9;
+            return Math.max(depreciatedPrice, 0);
+        });
+        res.status(200).send(`updated`);
+
+    } catch (error) {
+        res.status(500).send({ error: `unknown error in update value` });
+    }
+}
+
+//delete
+export function deleteCar(req: Request, res: Response) {
+    try {
+        const carId = parseInt(req.params.id, 10);
+        if (isNaN(carId)) return res.status(400).send({ error: "invalid car id" })
+        const carIndex = cars.findIndex(c => c.id === carId)
+        if (!carIndex) return res.status(404).send({ error: "could not find car" })
+        cars.splice(carIndex, 1)
+        res.status(200).send({ ok: true })
+    } catch (error) {
+        res.status(500).send({ error: "unknown error in deleteCar" });
     }
 }
--- 03-node-js/08-routes-08-09-2025/HomePuzzle/Ayala/src/model/car.model.ts
+++ 03-node-js/08-routes-08-09-2025/HomePuzzle/Ayala/src/model/car.model.ts
@@ -1,13 +1,22 @@
 export interface Car {
     id: number;
     brand: string;
-    model:string;
-    year:number;
+    model: string;
+    year: number;
     price: number;
+    color: string;
+    mileage: number;
+    isAvailable: boolean;
 }
 
+export enum Status {
+    AVAILABLE = "Available",
+    SOLD = "Sold",
+    RESERVED = "Reserved"
+
+}
 export const cars: Car[] = [
-    { id: 1, brand: "Toyota", model: "Camry", year: 2020, price: 24000 },
-    { id: 2, brand: "Honda", model: "Civic", year: 2019, price: 22000 },
-    { id: 3, brand: "Ford", model: "Mustang", year: 2021, price: 26000 }
+    { id: 1, brand: "Toyota", model: "Camry", year: 2020, price: 24000, color: "Red", mileage: 15000, isAvailable: true },
+    { id: 2, brand: "Honda", model: "Civic", year: 2019, price: 22000, color: "Blue", mileage: 20000, isAvailable: false },
+    { id: 3, brand: "Ford", model: "Mustang", year: 2021, price: 26000, color: "Black", mileage: 10000, isAvailable: true }
 ];
