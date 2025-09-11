d5a7fe8fb0b317c0eb6dc117fa80c434c2ff719d
Tal Yaron <tal.yaron@gmail.com>
2025-09-11 21:45:14 +0300

    Configures MongoDB URI using environment variables
    Loads environment variables using dotenv.
    
    Updates the MongoDB connection string to use the MONGODB_URI environment variable.
    This allows for more flexible configuration of the database connection,
    especially in deployment environments.
    
    Adds a check to ensure the MONGODB_URI is defined, exiting with an error
    if it is not to prevent application crashes.

--- 04-MongoDB/01-CRUD-11-09-2025/example/src/index.ts
+++ 04-MongoDB/01-CRUD-11-09-2025/example/src/index.ts
@@ -1,12 +1,22 @@
 import express, { Express, Request, Response } from 'express';
 import path from 'path';
 import mongoose from 'mongoose';
+import dotenv from 'dotenv';
+
+dotenv.config();
 
 const app: Express = express();
 const port = 3000;
 
+const MONGODB_URI = process.env.MONGODB_URI || '';
 
-mongoose.connect('mongodb+srv://tal:jbzulqpuzkB0PZKB@cluster0.0hzknon.mongodb.net/fs-apr25').then(() => {
+if (!MONGODB_URI) {
+    console.error("MONGODB_URI is not defined in environment variables");
+    process.exit(1);
+}
+
+
+mongoose.connect(`${MONGODB_URI}/fs-apr25`).then(() => {
     console.log("Connected to MongoDB");
 }).catch(err => {
     console.error("Failed to connect to MongoDB", err);
@@ -16,6 +26,7 @@ app.use(express.json());
 app.use(express.static(path.join(__dirname, '../public')));
 
 import studentRoutes from './routes/students.route';
+
 app.use('/api/students', studentRoutes);
 
 app.listen(port, () => {
