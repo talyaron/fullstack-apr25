67025b83a4a25494c78c3ab43c91a3448f1cbc96
Razanmani <razan.mani97@gmail.com>
2025-09-11 20:26:36 +0300

    connecting mongoose

--- 04-MongoDB/01-CRUD-11-09-2025/followup/Razan/src/index.ts
+++ 04-MongoDB/01-CRUD-11-09-2025/followup/Razan/src/index.ts
@@ -4,6 +4,12 @@ import path from 'path';
 const app: Express = express();
 const port = 3000;
 
+mongoose.connect('').then(() => {
+  console.log("Connected to MongoDB");
+}).catch(err =>{
+  console.error("Failed to connect to MongoDB", err);
+})
+
 app.use(express.json());
 app.use(express.static(path.join(__dirname, '../public')));
 
