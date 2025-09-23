4655790eb411b1d8913e45529076d9a6cbba8936
סהר שבת <saharshabat@MacBook-Air-sl-shr.local>
2025-09-23 18:40:49 +0300

    sahar practice

---
+++ 04-MongoDB/01-CRUD-11-09-2025/followup/sahar/src/routes/students.route.ts
@@ -0,0 +1,80 @@
+import express from 'express'
+import IStudent, {Student} from '../model/student.model';
+import { getAboveTwenty } from '../controllers/Controllers';
+const router = express.Router();
+
+router.get("/all-students", async (_req, res) => {
+    try {
+        const students = await IStudent.find({});
+        res.status(200).send({ students });
+    } catch (error: any) {
+        res.status(500).send({ error: `Failed to fetch students: ${error.message}` });
+    }
+});
+
+router.get("/all-above-twenty",getAboveTwenty);
+
+
+router.post("/add-student", async (req, res) => {
+    try {
+        console.log("Request Body:", req.body);
+        const newStudent = req.body as Student;
+        
+        const student = new IStudent(newStudent);
+        
+        await student.save();
+        console.log("Student saved to database");
+        res.status(201).send(`Student ${newStudent.name} added successfully`);
+    } catch (err: any) {
+        console.error("Error saving student to database", err);
+        res.status(500).send(`Error saving student to database: ${err.message}`);
+    }
+});
+
+router.put("/update-student", async (req, res) => {
+    try {
+        const { age } = req.body;
+        const id = req.query.id as string;
+        console.log("id:", id, "age:", age);
+
+        if (!id || typeof age !== 'number') {
+            return res.status(400).send("Invalid request data, id and age must be provided");
+        }
+
+        const updatedStudent = await IStudent.findByIdAndUpdate(
+            id,
+            { age },
+            { new: true }
+        );
+
+        if (!updatedStudent) {
+            return res.status(404).send("Student not found");
+        }
+
+        res.status(200).send(`Student ${updatedStudent.name} updated successfully`);
+    } catch (error: any) {
+        res.status(500).send({ error: `Internal Server Error: ${error.message}` });
+    }
+});
+
+router.delete("/delete-student", async (req, res) => {
+    try {
+        const id = req.query.id as string;
+
+        if (!id) {
+            return res.status(400).send("Invalid request data, id must be provided");
+        }
+
+        const deletedStudent = await IStudent.findByIdAndDelete(id);
+        
+        if (!deletedStudent) {
+            return res.status(404).send("Student not found");
+        }
+
+        res.status(200).send(`Student ${deletedStudent.name} deleted successfully`);
+    } catch (error: any) {
+        res.status(500).send({ error: `Internal Server Error: ${error.message}` });
+    }
+});
+
+export default router;  
