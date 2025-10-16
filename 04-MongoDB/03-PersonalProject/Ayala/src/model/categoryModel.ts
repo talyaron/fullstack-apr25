26ac548172d263bee79e04be8cebd4dfaec9f4e4
Ayala Halevi <ayalahalevi2@gmail.com>
2025-10-16 16:51:22 +0300

    personal project routs

--- 04-MongoDB/03-PersonalProject/Ayala/src/model/categoryModel.ts
+++ 04-MongoDB/03-PersonalProject/Ayala/src/model/categoryModel.ts
@@ -1,12 +1,15 @@
-export interface Category{
-    id:string;
-    name:string;
+import { Document, model, Schema } from "mongoose";
+
+export interface Category extends Document {
+    name: string;
 }
 
-import { model, Schema } from "mongoose";
+
 export const categorySchema = new Schema({
-    id:{type: String, required: true},
-    name:{type:String,  required: true}
+    name: {
+        type: String,
+        required: true
+    }
 })
 export const ICategory = model('Category', categorySchema)
 
