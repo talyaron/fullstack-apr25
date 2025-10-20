f22139965d2f24522e6812002ad0bc192154adac
Ayala Halevi <ayalahalevi2@gmail.com>
2025-10-20 17:20:52 +0300

    clas followup

--- 04-MongoDB/03-PersonalProject/Ayala/src/routes/recipeRouts.ts
+++ 04-MongoDB/03-PersonalProject/Ayala/src/routes/recipeRouts.ts
@@ -6,7 +6,7 @@ router.get("/get-recipes-by-category", recipeController.getRecipesByCategory)
     .get("/get-recipe/:id", recipeController.getRecipe)
     .get("/get-favorite-recipes", recipeController.getFavoriteRecipes)
     .post("/add-recipe/", recipeController.addRecipe)
-    // .patch("/edit-recipe", recipeController.editRecipe)
+    // .patch("/edit-recipe:id", recipeController.editRecipe)
     // .patch("/mark-as-favorite", recipeController.markAsFavorite)
     // .delete("/delete-recipe", recipeController.deleteRecipe)
 
