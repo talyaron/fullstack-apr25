1f7c06c7e60ec30b31a4783a50b4eb7112ada8f8
amit reuveni <amitr123r@gmail.com>
2025-10-22 20:02:24 +0300

    add basic services add basic controller add cookieParser to index.ts

--- 04-MongoDB/04-TeamProject-20-10-2025/wordle/src/index.ts
+++ 04-MongoDB/04-TeamProject-20-10-2025/wordle/src/index.ts
@@ -1,6 +1,7 @@
 import express, { Express } from 'express';
 import path from "path";
 import dotenv from "dotenv";
+import cookieParser from 'cookie-parser';
 
 
 dotenv.config();
@@ -12,6 +13,7 @@ const port = 3000;
 
 app.use(express.json());
 app.use(express.static(path.join(__dirname, '../public')));
+app.use(cookieParser());
 
 app.use((req, _, next) => {
   console.log("➡️", req.method, req.url);
