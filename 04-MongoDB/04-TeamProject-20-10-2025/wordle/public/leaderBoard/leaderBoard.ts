fce7693ec74d3ee69fe1f0e874cb770f75c340bc
amit reuveni <amitr123r@gmail.com>
2025-10-30 19:59:40 +0200

    add leaderBoard controller add scss to leaderBoard add a little bit function's in leaderBoard.ts

---
+++ 04-MongoDB/04-TeamProject-20-10-2025/wordle/public/leaderBoard/leaderBoard.ts
@@ -0,0 +1,25 @@
+async function fetchLeaderBoard() {
+    try {
+        const response = await fetch('/api/data/leaderboard');
+        if (!response.ok) {
+            throw new Error(`Network response was not ok can't find leaderboard data`);
+        }
+        const data = await response.json();
+        renderLeaderBoard(data);
+    } catch (error) {
+        console.error('Error fetching leaderboard:', error);
+    }
+}
+function renderLeaderBoard(data : Array<{ username: string; score: number; attempts: number; date: string; }>) {
+    const list = document.getElementById('leaderboard-list');
+    if (!list) return;
+
+    list.innerHTML = "";
+
+    data.forEach(player => {
+        const listItem = document.createElement('li');
+        listItem.textContent = `${player.username} - Score: ${player.score}, Attempts: ${player.attempts}, Date: ${player.date}`;
+        list.appendChild(listItem);
+    });
+
+}
