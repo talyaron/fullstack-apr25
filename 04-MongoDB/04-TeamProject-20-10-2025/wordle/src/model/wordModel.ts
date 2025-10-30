659fd6c3c803eb42e50a52d97df4e3f7e21e96b1
Ayala Halevi <ayalahalevi2@gmail.com>
2025-10-31 00:38:38 +0200

    fixing middleware and combining to the needed routes

--- 04-MongoDB/04-TeamProject-20-10-2025/wordle/src/model/wordModel.ts
+++ 04-MongoDB/04-TeamProject-20-10-2025/wordle/src/model/wordModel.ts
@@ -7,6 +7,6 @@ export type Word = {
 
 const wordSchema = new Schema<Word>({
   word: { type: String, required: true, unique: true, trim: true },
-}, { timestamps: true });
+});
 
 export const wordModel = model("Word", wordSchema);
--- 04-MongoDB/04-TeamProject-20-10-2025/wordle/src/routes/dataRoutes.ts
+++ 04-MongoDB/04-TeamProject-20-10-2025/wordle/src/routes/dataRoutes.ts
@@ -1,11 +1,11 @@
 import express from "express";
 import * as dataControllers from '../controllers/dataControllers'
-
+import {anonymousUserMiddleware} from '../middleware/middleware'
 const router = express.Router();
 
 
-router.get("/get-user-data", dataControllers.getUserData)
-      .patch("/update-data", dataControllers.updateUserData)
-      .get("/leaderboard", dataControllers.getLeaderBoard);
+router.get("/get-user-data",anonymousUserMiddleware,  dataControllers.getUserData)
+      .patch("/update-data", anonymousUserMiddleware, dataControllers.updateUserData)
+      .get("/leaderboard", anonymousUserMiddleware, dataControllers.getLeaderBoard);
 
 export default router;
