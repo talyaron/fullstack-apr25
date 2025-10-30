c7ee6a40c818fcd2b1be4e1bbe19c9fedacf2aec
Ayala Halevi <ayalahalevi2@gmail.com>
2025-10-31 00:39:19 +0200

    fixing get user data controller (creating the start)

--- 04-MongoDB/04-TeamProject-20-10-2025/wordle/src/controllers/dataControllers.ts
+++ 04-MongoDB/04-TeamProject-20-10-2025/wordle/src/controllers/dataControllers.ts
@@ -1,20 +1,27 @@
 import { Request, Response } from "express";
 import { dataModel } from "../model/dataModel";
 import { populate } from "dotenv";
-
 export const getUserData = async (req: Request, res: Response) => {
   try {
-    const { userId } = req.query;
+      const userId = res.locals.userId;
+
     const data = await dataModel.findOne({ userId });
+    console.log("getting data:" , userId)
+    if(!data){
+      const data = await dataModel.create({userId,amountOfGames:0, amountOfVictories: 0} );
+      res.status(201).json({ message: "Userdata added successfully", data });
+      return;}
     res.json(data);
   } catch {
     res.status(500).json({ message: "Error fetching data" });
   }
 };
 
+
 export const updateUserData = async (req: Request, res: Response) => {
   try {
-    const { userId } = req.query;
+      const userId = res.locals.userId;
+
     const { amountOfGames, amountOfVictories } = req.body;
     const updated = await dataModel.findOneAndUpdate(
       { userId },
