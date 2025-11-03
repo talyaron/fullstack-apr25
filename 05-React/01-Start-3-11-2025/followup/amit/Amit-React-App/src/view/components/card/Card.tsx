e28c18460a3740c0ef5bbd14b73b945355b9a1cb
amit reuveni <amitr123r@gmail.com>
2025-11-03 20:37:27 +0200

    played with react

---
+++ 05-React/01-Start-3-11-2025/followup/amit/Amit-React-App/src/view/components/card/Card.tsx
@@ -0,0 +1,12 @@
+import "./Card.scss";
+
+export function Card() {
+  return (
+    <div className="card">
+      <h3 className="card-title">My Card</h3>
+      <p className="card-description">lorem ipsum dolor sit amet</p>
+    </div>
+  )
+}
+
+export default Card
