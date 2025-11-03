75ae00c2da8f2f9448fb44da78bc773841d8d96b
Sahar Shabat <saharshabat@gmail.com>
2025-11-03 23:41:30 +0200

    sahar blog start

---
+++ 05-React/01-Start-3-11-2025/home puzzle/saharPersonalBlog/sahar/src/App.tsx
@@ -0,0 +1,14 @@
+import Carousel from "./components/Carousel"
+function App() {
+
+  return (
+<div className='mainContainer'>
+
+<h1>Welcome to my personal blog</h1>
+<Carousel/>
+
+</div>
+  );
+}
+
+export default App
