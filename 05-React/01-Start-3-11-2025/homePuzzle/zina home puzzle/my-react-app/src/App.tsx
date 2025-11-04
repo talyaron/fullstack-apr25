89854c0d168cfd5d31802b99aa5742fe9f2c04be
sahar <saharshabat@gmail.com>
2025-11-04 11:25:55 +0200

    bug fix

--- 05-React/01-Start-3-11-2025/homePuzzle/zina home puzzle/my-react-app/src/App.tsx
+++ 05-React/01-Start-3-11-2025/homePuzzle/zina home puzzle/my-react-app/src/App.tsx
@@ -1,7 +1,7 @@
 import { useState } from 'react';
 import reactLogo from './assets/react.svg';
 import viteLogo from '/vite.svg';
-import './Card.module.scss';
+import './cardfile/Card.scss';
 
 function App() {
   const [count, setCount] = useState(0);
