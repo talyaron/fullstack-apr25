e7c982dee5fd9b7fa207f0697cc4655cac252dd8
amit reuveni <amitr123r@gmail.com>
2025-11-13 21:05:17 +0200

    finish book puzzle

---
+++ 05-React/05-query-13-11-2025/classPuzzleBooks/amit/example-server-server/src/models/User.ts
@@ -0,0 +1,86 @@
+import mongoose, { Document, Schema, Model } from 'mongoose';
+import bcrypt from 'bcrypt';
+
+// User interface
+export interface IUser extends Document {
+  email: string;
+  password: string;
+  name: string;
+  createdAt: Date;
+  updatedAt: Date;
+  comparePassword(candidatePassword: string): Promise<boolean>;
+}
+
+// User Schema
+const userSchema = new Schema<IUser>(
+  {
+    email: {
+      type: String,
+      required: [true, 'Email is required'],
+      unique: true,
+      lowercase: true,
+      trim: true,
+      match: [
+        /^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/,
+        'Please provide a valid email'
+      ]
+    },
+    password: {
+      type: String,
+      required: [true, 'Password is required'],
+      minlength: [6, 'Password must be at least 6 characters long'],
+      select: false // Don't include password in queries by default
+    },
+    name: {
+      type: String,
+      required: [true, 'Name is required'],
+      trim: true,
+      minlength: [2, 'Name must be at least 2 characters long'],
+      maxlength: [50, 'Name cannot exceed 50 characters']
+    }
+  },
+  {
+    timestamps: true
+  }
+);
+
+// Index for faster queries
+userSchema.index({ email: 1 });
+
+// Hash password before saving
+userSchema.pre('save', async function(next) {
+  // Only hash the password if it has been modified (or is new)
+  if (!this.isModified('password')) {
+    return next();
+  }
+
+  try {
+    // Generate salt and hash password
+    const salt = await bcrypt.genSalt(10);
+    this.password = await bcrypt.hash(this.password, salt);
+    next();
+  } catch (error: any) {
+    next(error);
+  }
+});
+
+// Instance method to compare passwords
+userSchema.methods.comparePassword = async function(candidatePassword: string): Promise<boolean> {
+  try {
+    return await bcrypt.compare(candidatePassword, this.password);
+  } catch (error) {
+    return false;
+  }
+};
+
+// Remove password from JSON output
+userSchema.methods.toJSON = function() {
+  const obj = this.toObject();
+  delete obj.password;
+  delete obj.__v;
+  return obj;
+};
+
+const User: Model<IUser> = mongoose.model<IUser>('User', userSchema);
+
+export default User;
