20eca90b7208f0d770acef27f41263a5798f593a
Ayala Halevi <ayalahalevi2@gmail.com>
2025-11-20 18:34:57 +0200

    follow up links to rout

--- 05-React/06-routing-20-11-2025/followup/Ayala/src/view/pages/about/About.tsx
+++ 05-React/06-routing-20-11-2025/followup/Ayala/src/view/pages/about/About.tsx
@@ -1,8 +1,13 @@
+import { Link } from "react-router"
 
 const About = () => {
-  return (
-    <div>About</div>
-  )
+    return (
+        <div>About
+            <Link to="/"><button>Home</button></Link>
+            <Link to="/profile"><button>Profile</button></Link>
+        </div>
+
+    )
 }
 
 export default About
