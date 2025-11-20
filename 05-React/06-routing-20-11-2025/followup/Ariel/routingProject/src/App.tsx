6351fbbb5c694ff7c9465968e8de2890c7b8c13f
Ariel71232 <ari71232@outlook.com>
2025-11-20 18:34:55 +0200

    Added <Link/>
    Just added my #1 react-route link!

--- 05-React/06-routing-20-11-2025/followup/Ariel/routingProject/src/App.tsx
+++ 05-React/06-routing-20-11-2025/followup/Ariel/routingProject/src/App.tsx
@@ -2,6 +2,7 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.scss'
+import { Link } from 'react-router'
 
 function App() {
   const [count, setCount] = useState(0)
@@ -21,6 +22,9 @@ function App() {
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
         </button>
+        <Link to="/profile">
+          <button>Go To Profile</button>
+        </Link>
         <p>
           Edit <code>src/App.tsx</code> and save to test HMR
         </p>
