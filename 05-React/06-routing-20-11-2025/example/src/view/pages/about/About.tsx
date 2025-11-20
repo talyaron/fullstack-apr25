880e44ed8356d6eebc00e92b6b876994ad4dc1fe
Tal Yaron <tal.yaron@gmail.com>
2025-11-20 18:31:28 +0200

    link

--- 05-React/06-routing-20-11-2025/example/src/view/pages/about/About.tsx
+++ 05-React/06-routing-20-11-2025/example/src/view/pages/about/About.tsx
@@ -1,10 +1,14 @@
+import { Link } from "react-router"
 
 const About = () => {
-  return (
-    <div>
-        <h1>About page</h1>
-    </div>
-  )
+    return (
+        <div>
+            <h1>About page</h1>
+            <Link to="/">
+                <button>Go To Home</button>
+            </Link>
+        </div>
+    )
 }
 
 export default About
