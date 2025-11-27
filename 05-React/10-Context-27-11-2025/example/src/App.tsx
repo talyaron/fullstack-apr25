4bd5bcbac74b65f753128ef8f19b559716ae5ea9
Tal Yaron <tal.yaron@gmail.com>
2025-11-27 18:34:56 +0200

    context followup

--- 05-React/10-Context-27-11-2025/example/src/App.tsx
+++ 05-React/10-Context-27-11-2025/example/src/App.tsx
@@ -1,13 +1,17 @@
-import { useState } from 'react'
+import { useContext, useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import { ThemeContext } from './model/theme/ThemeProvider'
+import ToggleTheme from './view/components/toggleTheme/ToggleTheme'
 
 function App() {
   const [count, setCount] = useState(0)
 
+  const { theme } = useContext(ThemeContext);
+
   return (
-    <>
+    <div className={`page App ${theme}`}>
       <div>
         <a href="https://vite.dev" target="_blank">
           <img src={viteLogo} className="logo" alt="Vite logo" />
@@ -28,7 +32,8 @@ function App() {
       <p className="read-the-docs">
         Click on the Vite and React logos to learn more
       </p>
-    </>
+      <ToggleTheme />
+    </div>
   )
 }
 
