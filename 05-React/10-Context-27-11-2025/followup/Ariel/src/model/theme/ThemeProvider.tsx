e3d99f9a4eac6f7826024d4fbf00dacab46f0c16
Ariel71232 <ari71232@outlook.com>
2025-11-27 18:51:48 +0200

    My F.U for context 😄

---
+++ 05-React/10-Context-27-11-2025/followup/Ariel/src/model/theme/ThemeProvider.tsx
@@ -0,0 +1,23 @@
+import { createContext, useState } from "react";
+
+// app state
+const ThemeContext = createContext({
+  theme: "light" as "light" | "dark",
+  handleToggleTheme: () => {},
+});
+
+function ThemeProvider({ children }: { children: React.ReactNode }) {
+    const [theme, setTheme] = useState<"light" | "dark">("light");
+
+    const handleToggleTheme = () => {
+        setTheme((prevTheme) => (prevTheme === "light" ? "dark" : "light"));
+    };
+
+    return (
+        <ThemeContext.Provider value={{ theme, handleToggleTheme }}>
+            {children}
+        </ThemeContext.Provider>
+    );
+}
+
+export { ThemeProvider, ThemeContext };
