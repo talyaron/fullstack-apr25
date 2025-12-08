cd3882c620c2f7f8b17248a618c721e088a294ee
Tal Yaron <tal.yaron@gmail.com>
2025-12-08 18:42:00 +0200

    redux

---
+++ 05-React/11-Redux-8-12-2025/example/src/view/components/counterView/CounterView.tsx
@@ -0,0 +1,14 @@
+
+import { useSelector } from 'react-redux'
+import type { RootState } from '../../../redux/store'
+
+const CounterView = () => {
+
+const count = useSelector((state: RootState) => state.counter.value)
+
+  return (
+    <div>This is the counter: {count}</div>
+  )
+}
+
+export default CounterView;
